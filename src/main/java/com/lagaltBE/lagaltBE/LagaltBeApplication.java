bdf091040695795aa292443f439bf5597d38188d
Sondre Melhus <42571126+SondreEMelhus@users.noreply.github.com>
2022-10-03 15:01:18 +0200

    Initial commit

---
+++ src/main/java/com/lagaltBE/lagaltBE/LagaltBeApplication.java
@@ -0,0 +1,13 @@
+package com.lagaltBE.lagaltBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagaltBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagaltBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lagaltBE/lagaltBE/LagaltBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagaltBE.lagaltBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaltBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
