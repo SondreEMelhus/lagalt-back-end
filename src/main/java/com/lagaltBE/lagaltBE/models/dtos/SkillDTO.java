5000f9ceeb86a2e792f0b4f9536546d6737ad215
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-10-05 14:39:39 +0200

    Add SkillDTO

---
+++ src/main/java/com/lagaltBE/lagaltBE/models/dtos/SkillDTO.java
@@ -0,0 +1,14 @@
+package com.lagaltBE.lagaltBE.models.dtos;
+
+import com.lagaltBE.lagaltBE.models.Skill;
+import com.lagaltBE.lagaltBE.models.UserAccount;
+import lombok.Data;
+import java.util.Set;
+
+@Data
+public class SkillDTO {
+    private int id;
+    private String title;
+    private Set<UserAccount> userAccounts;
+    private Set<Skill> skills;
+}
