6f46e69f13aaa55f7f4a7a03c85537ac993709f2
Trygve Johannessen <trygvejohannessen88@gmail.com>
2022-10-05 21:43:29 +0200

    project dto update

--- src/main/java/com/lagaltBE/lagaltBE/models/dtos/ProjectDTO.java
+++ src/main/java/com/lagaltBE/lagaltBE/models/dtos/ProjectDTO.java
@@ -2,6 +2,9 @@ package com.lagaltBE.lagaltBE.models.dtos;
 
 import lombok.Data;
 
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
 @Data
 public class ProjectDTO {
     private int id;
