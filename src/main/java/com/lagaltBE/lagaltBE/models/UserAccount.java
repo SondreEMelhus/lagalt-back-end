bacb1bf9b83fdc33483e8860a0a4bd118d748055
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-10-05 14:41:48 +0200

    add skill set

--- src/main/java/com/lagaltBE/lagaltBE/models/UserAccount.java
+++ src/main/java/com/lagaltBE/lagaltBE/models/UserAccount.java
@@ -3,6 +3,7 @@ package com.lagaltBE.lagaltBE.models;
 import lombok.Getter;
 import lombok.Setter;
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -12,11 +13,17 @@ public class UserAccount {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(length = 50, nullable = false)
-    String name;
+    private String name;
     @Column(length = 100, nullable = false)
-    String email;
+    private String email;
+    @ManyToMany
+    @JoinTable(
+            name = "user_account_skill",
+            joinColumns = {@JoinColumn(name = "user_account_id")},
+            inverseJoinColumns = {@JoinColumn(name = "skill_id")}
+    )
+    private Set<Skill> skills;
     // Set<Application> applicationHistory;
-    // Set<Skill> skills;
     // Set<Contribution> contributions;
     // Set<Contributor> contributor;
 }
