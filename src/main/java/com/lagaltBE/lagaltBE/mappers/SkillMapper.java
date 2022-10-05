cd740c5b3b64c30a7d0c75c29d8d014c68775803
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-10-05 14:39:21 +0200

    Add SkillMapper

---
+++ src/main/java/com/lagaltBE/lagaltBE/mappers/SkillMapper.java
@@ -0,0 +1,14 @@
+package com.lagaltBE.lagaltBE.mappers;
+
+import com.lagaltBE.lagaltBE.models.Skill;
+import com.lagaltBE.lagaltBE.models.dtos.SkillDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public abstract class SkillMapper {
+    public abstract SkillDTO skillToSkillDto(Skill skill);
+
+    @Mapping(target = "title")
+    public abstract Skill skillDtoToUSkill(SkillDTO skillDTO);
+}
