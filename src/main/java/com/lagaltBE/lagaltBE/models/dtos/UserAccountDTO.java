02aef8fc686b82483e61afb995b82a5e8a5f664c
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-10-05 11:14:09 +0200

    add user DTO

---
+++ src/main/java/com/lagaltBE/lagaltBE/models/dtos/UserAccountDTO.java
@@ -0,0 +1,10 @@
+package com.lagaltBE.lagaltBE.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class UserAccountDTO {
+    private int id;
+    private String name;
+    private String email;
+}
