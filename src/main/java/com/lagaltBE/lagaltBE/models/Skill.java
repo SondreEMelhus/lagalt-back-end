def722aa1022f135d3b077bf554b1b594265c0fc
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-10-05 14:39:52 +0200

    Add Skill

---
+++ src/main/java/com/lagaltBE/lagaltBE/models/Skill.java
@@ -0,0 +1,20 @@
+package com.lagaltBE.lagaltBE.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Skill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 100, nullable = false)
+    private String title;
+    @ManyToMany(mappedBy = "skills")
+    private Set<UserAccount> userAccounts;
+}
