a826096459af99b271c9965add4a3eef323f8081
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-10-05 11:14:57 +0200

    add test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into project (name) values ('spille cs');
 insert into project (name) values ('fotballlag');
 insert into project (name) values ('larping');
+
+insert into user_account (name, email) values ('Ulrik', 'ulrik@gmail.com');
+insert into user_account (name, email) values ('Trygve', 'trygve@gmail.com');
