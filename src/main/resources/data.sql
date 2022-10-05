313a1298ee66bde910e2478ee9d854092673f511
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-10-05 14:41:14 +0200

    add test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,12 @@
-insert into project (name) values ('spille cs');
-insert into project (name) values ('fotballlag');
-insert into project (name) values ('larping');
+INSERT INTO project (name) VALUES ('spille cs');
+INSERT INTO project (name) VALUES ('fotballlag');
+INSERT INTO project (name) VALUES ('larping');
 
-insert into user_account (name, email) values ('Ulrik', 'ulrik@gmail.com');
-insert into user_account (name, email) values ('Trygve', 'trygve@gmail.com');
+INSERT INTO user_account (name, email) VALUES ('Ulrik', 'ulrik@gmail.com');
+INSERT INTO user_account (name, email) VALUES ('Trygve', 'trygve@gmail.com');
+
+INSERT INTO skill (title) VALUES ('Java');
+INSERT INTO skill (title) VALUES ('Design');
+
+INSERT INTO user_account_skill ("user_account_id", "skill_id") VALUES (1,1);
+INSERT INTO user_account_skill ("user_account_id", "skill_id") VALUES (1,2);
