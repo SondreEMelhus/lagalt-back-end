7a180735463d386903bf791217139c9544aafd84
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-10-06 14:26:02 +0200

    update mapper

--- src/main/java/com/lagaltBE/lagaltBE/mappers/UserAccountMapper.java
+++ src/main/java/com/lagaltBE/lagaltBE/mappers/UserAccountMapper.java
@@ -1,15 +1,27 @@
 package com.lagaltBE.lagaltBE.mappers;
 
+import com.lagaltBE.lagaltBE.models.Skill;
 import com.lagaltBE.lagaltBE.models.UserAccount;
 import com.lagaltBE.lagaltBE.models.dtos.UserAccountDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public abstract class UserAccountMapper {
+    @Named("skillsToIds")
+    Set<Integer> map(Set<Skill> source) {
+        if(source == null)
+            return null;
+        return source.stream()
+                .map(s -> s.getId()).collect(Collectors.toSet());
+    }
 
+    @Mapping(target = "skills", source = "skills", qualifiedByName = "skillsToIds")
     public abstract UserAccountDTO userAccountToUserAccountDto(UserAccount userAccount);
 
-    @Mapping(target = "name")
+    @Mapping(target = "name", ignore = true)
     public abstract UserAccount userAccountDtoToUserAccount(UserAccountDTO userAccountDTO);
 }
