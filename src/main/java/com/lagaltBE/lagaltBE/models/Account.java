b95e9b455aedb009cbe9b1ad3aba405fda06f1d7
trygve88 <trygvejohannessen88@gmail.com>
2022-10-11 21:58:54 +0200

    ingenting i account må være unikt (for test8ing)

--- src/main/java/com/lagaltBE/lagaltBE/models/Account.java
+++ src/main/java/com/lagaltBE/lagaltBE/models/Account.java
@@ -18,9 +18,9 @@ public class Account {
     private String firstName;
     @Column(length = 50)
     private String lastName;
-    @Column(length = 50, nullable = false, unique = true)
+    @Column(length = 50, nullable = false) //, unique = true)
     private String username;
-    @Column(length = 100, nullable = false, unique = true)
+    @Column(length = 100, nullable = false) //, unique = true)
     private String email;
     @Column()
     private boolean visible;
